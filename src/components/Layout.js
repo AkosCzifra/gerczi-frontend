fa081f9f9dfae7cc80fecdcd4a4529aa01dcb532
Akos Czifra <akosczifraa@gmail.com>
2020-05-06 19:17:58 +0200

    renamed Nav to NavBar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Nav from './navbar/Nav'
+import NavBar from './navbar/NavBar'
 import Footer from './Footer'
 
 const Layout = (props) => {
     return (
         <React.Fragment>
-            <Nav />
+            <NavBar />
             <main>
                 {props.children}
             </main>
