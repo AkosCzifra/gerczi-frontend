6625600ce537d7fd21a27a4e36229d454d7ea49e
Akos Czifra <akosczifraa@gmail.com>
2020-05-06 18:43:58 +0200

    created basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import Layout from './components/Layout'
+import Home from './pages/home/Home'
+import Menu from './pages/menu/Menu'
+import Contact from './pages/contact/Contact'
+import About from './pages/about/About'
+import PageNotFound from './pages/page-not-found/PageNotFound'
+
+const App = () => {
   return (
     <div className="App">
-
+      <Layout>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/" exact component={Home} />
+          <Route path="/page-not-found" component={PageNotFound} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
