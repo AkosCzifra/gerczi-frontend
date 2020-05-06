e7369dd55d81065f416b4041386b12ff0cf93e65
Akos Czifra <akosczifraa@gmail.com>
2020-05-06 18:47:02 +0200

    created Nav component

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = (props) => {
+    return (
+        <header>
+            <p>Logo</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Login</p>
+            <p>Icon</p>
+        </header>
+    )
+}
+
+export default Nav;
