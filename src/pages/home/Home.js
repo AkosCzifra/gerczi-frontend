f9e0fad8fb10ad76d6045c6bac931cfe383b4ae4
gergicsmilan <gergics.milan95@gmail.com>
2020-05-12 10:15:33 +0200

    created basic http request to server (with axios)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import axios from '../../axios';
 
 const Home = () => {
-    return (
-        <h1>Home</h1>
-    );
-}
+  useEffect(() => {
+    const getTestRequest = async () => {
+      try {
+        const response = await axios.get('/');
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getTestRequest();
+  }, []);
+
+  return <h1>Home</h1>;
+};
 
 export default Home;
--- src/pages/menu/Menu.js
+++ src/pages/menu/Menu.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from '../../axios';
+import ErrorHandler from '../../components/errorHandler/ErrorHandler';
+import styled from 'styled-components';
+
+const Test = styled.div``;
 
 const Menu = () => {
-    return (
-        <h1>Menu</h1>
-    );
-}
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const result = await axios.get('/categories');
+        if (result && result.data.data) {
+          setCategories(result.data.data);
+        }
+      } catch (err) {
+        setError(err);
+      }
+    };
+    getCategories();
+  }, []);
+
+  if (error) return <ErrorHandler error={error} />;
+
+  if (!categories) return <h1>loading...</h1>; //kell pörgőfasz
+
+  return (
+    <Test>
+      {categories.map((category) => (
+        <h1 key={category._id}>{category.name}</h1>
+      ))}
+    </Test>
+  );
+};
 
 export default Menu;
