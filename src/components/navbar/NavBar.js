3bd98c1edd9b7451710246accb7086b742fbf453
Akos Czifra <akosczifraa@gmail.com>
2020-05-14 23:03:20 +0200

    implemented Navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import styled from 'styled-components'
+
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 import NavItems from './NavItems'
+import NavIcon from './NavIcon'
+import NavLogo from './NavLogo'
+import NavLink from './NavLink'
+import DropdownMenu from './DropdownMenu';
 
-const NavBar = (props) => {
+
+
+const Nav = styled.nav`
+  height: var(--nav-size);
+  background-color: var(--bg);
+  padding: 0 1rem;
+  border-bottom: var(--border);
+  display: flex;
+  justify-content: space-between;
+`
+
+function Navbar() {
     return (
-        <header>
-            <NavItems />
-        </header>
+        <Nav>
+            <NavLogo url="/"></NavLogo>
+            <NavItems>
+                <NavLink name="Menu" url="/menu"></NavLink>
+                <NavLink name="Contact" url="/contact"></NavLink>
+                <NavLink name="About" url="/about"></NavLink>
+                <NavIcon icon={<AccountCircleIcon />} url="#">
+                    <DropdownMenu />
+                </NavIcon>
+                <NavIcon icon={<ShoppingCartIcon />} url="/cart"></NavIcon>
+            </NavItems>
+        </Nav>
     )
 }
 
-export default NavBar;
+export default Navbar;
