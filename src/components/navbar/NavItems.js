100f7d4bd84297a842ece0e6190e7c5617f84eae
Akos Czifra <akosczifraa@gmail.com>
2020-05-14 23:04:08 +0200

    implemented NavItems

--- src/components/navbar/NavItems.js
+++ src/components/navbar/NavItems.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import styled from 'styled-components'
 
-import NavItem from './NavItem'
 
-const NavItems = (props) => {
+
+const StyledUl = styled.ul`
+  max-width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  `
+
+function NavItems(props) {
     return (
-        <ul>
-            <NavItem url="/">Home</NavItem>
-            <NavItem url="/menu">Menu</NavItem>
-            <NavItem url="/contact">Contact</NavItem>
-            <NavItem url="/about">About</NavItem>
-        </ul>
+        <StyledUl>
+            {props.children}
+        </StyledUl>
     )
 }
 
