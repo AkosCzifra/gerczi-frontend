d1ee4ba453cff7f1dabb35a42ccc02c80d24795e
gergicsmilan <gergics.milan95@gmail.com>
2020-05-21 11:00:28 +0200

    created context for cart

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Layout from './components/Layout'
-import Home from './pages/home/Home'
-import Menu from './pages/menu/Menu'
-import Contact from './pages/contact/Contact'
-import About from './pages/about/About'
-import PageNotFound from './pages/page-not-found/PageNotFound'
+import CartContextProvider from './context/CartContext';
+import Layout from './components/Layout';
+import Home from './pages/home/Home';
+import Menu from './pages/menu/Menu';
+import Contact from './pages/contact/Contact';
+import About from './pages/about/About';
+import Cart from './pages/cart/Cart';
+import PageNotFound from './pages/page-not-found/PageNotFound';
 
 const App = () => {
   return (
-    <div className="App">
+    <CartContextProvider>
       <Layout>
         <Switch>
           <Route path="/page-not-found" component={PageNotFound} />
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
           <Route path="/menu" component={Menu} />
+          <Route path="/cart" component={Cart} />
           <Route path="/" exact component={Home} />
           <Redirect to="/page-not-found" />
         </Switch>
       </Layout>
-
-    </div>
+    </CartContextProvider>
   );
-}
+};
 
 export default App;
---
+++ src/context/CartContext.js
@@ -0,0 +1,64 @@
+import React, { createContext, useState } from 'react';
+import {
+  getCart,
+  addProductToCart,
+  incrementProductInCart,
+  decrementProductQuantity,
+  deleteProductFromCart,
+} from '../utils/cart-manager/cartManager';
+
+export const CartContext = createContext();
+
+const CartContextProvider = (props) => {
+  const [cart, setCart] = useState(getCart());
+
+  const addProduct = (product) => {
+    const modifiedCart = addProductToCart(product);
+    setCart(modifiedCart);
+  };
+
+  const increment = (id) => {
+    const modifiedCart = incrementProductInCart(id);
+    setCart(modifiedCart);
+  };
+
+  const decrement = (id) => {
+    const modifiedCart = decrementProductQuantity(id);
+    setCart(modifiedCart);
+  };
+
+  const remove = (id) => {
+    const modifiedCart = deleteProductFromCart(id);
+    setCart(modifiedCart);
+  };
+
+  const getSumQuantity = () => {
+    let sum = 0;
+    cart.forEach((item) => (sum = sum + item.quantity));
+    return sum;
+  };
+
+  const getSumPrice = () => {
+    let sum = 0;
+    cart.forEach((item) => (sum = sum + item.price * item.quantity));
+    return sum;
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart: cart,
+        addProduct: addProduct,
+        increment: increment,
+        decrement: decrement,
+        remove: remove,
+        getSumQuantity: getSumQuantity,
+        getSumPrice: getSumPrice,
+      }}
+    >
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
