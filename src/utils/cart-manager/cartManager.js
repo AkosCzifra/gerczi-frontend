bb16d70dd5cf9b1fc3482d2fd284aa2e5daaaebd
gergicsmilan <gergics.milan95@gmail.com>
2020-05-21 11:02:06 +0200

    added api endpoint config

--- src/utils/cart-manager/cartManager.js
+++ src/utils/cart-manager/cartManager.js
@@ -1,4 +1,4 @@
-const getCartFromLocalhost = () => {
+export const getCart = () => {
   let cart = [];
   if (localStorage.getItem('cart')) {
     cart = JSON.parse(localStorage.getItem('cart'));
@@ -6,24 +6,46 @@ const getCartFromLocalhost = () => {
   return cart;
 };
 
-export const addProductToCart = (id) => {
-  let cart = getCartFromLocalhost();
+export const addProductToCart = ({ _id, price, name, imageUrl }) => {
+  let cart = getCart();
 
   let contains = false;
   cart.map((item) => {
-    if (item.productId === id) {
+    if (item.productId === _id) {
       contains = true;
       return (item.quantity += 1);
     }
     return item;
   });
 
-  if (!contains) cart.push({ productId: id, quantity: 1 });
+  if (!contains)
+    cart.push({
+      productId: _id,
+      quantity: 1,
+      price: price,
+      name: name,
+      imageUrl: imageUrl,
+    });
   localStorage.setItem('cart', JSON.stringify(cart));
+  return cart;
+};
+
+export const incrementProductInCart = (id) => {
+  let cart = getCart();
+
+  cart.map((item) => {
+    if (item.productId === id) {
+      return (item.quantity += 1);
+    }
+    return item;
+  });
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  return cart;
 };
 
 export const decrementProductQuantity = (id) => {
-  let cart = getCartFromLocalhost();
+  let cart = getCart();
   if (cart.length === 0) return;
 
   cart.map((item) => {
@@ -34,12 +56,14 @@ export const decrementProductQuantity = (id) => {
   });
 
   localStorage.setItem('cart', JSON.stringify(cart));
+  return cart;
 };
 
 export const deleteProductFromCart = (id) => {
-  let cart = getCartFromLocalhost();
+  let cart = getCart();
   if (cart.length === 0) return;
 
   cart = cart.filter((item) => item.productId !== id);
   localStorage.setItem('cart', JSON.stringify(cart));
+  return cart;
 };
