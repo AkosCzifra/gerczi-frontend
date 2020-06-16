97f85e86f4f6718ce025d98e4d20ddaf8bc345ac
gergicsmilan <gergics.milan95@gmail.com>
2020-06-16 13:15:05 +0200

    added auth context provider to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import CartContextProvider from './context/CartContext';
+import AuthContextProvider from './context/AuthContext';
 import Layout from './components/Layout';
 import Home from './pages/home/Home';
 import Menu from './pages/menu/Menu';
@@ -12,19 +13,21 @@ import PageNotFound from './pages/page-not-found/PageNotFound';
 
 const App = () => {
   return (
-    <CartContextProvider>
-      <Layout>
-        <Switch>
-          <Route path="/page-not-found" component={PageNotFound} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
-          <Route path="/menu" component={Menu} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/" exact component={Home} />
-          <Redirect to="/page-not-found" />
-        </Switch>
-      </Layout>
-    </CartContextProvider>
+    <AuthContextProvider>
+      <CartContextProvider>
+        <Layout>
+          <Switch>
+            <Route path="/page-not-found" component={PageNotFound} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/page-not-found" />
+          </Switch>
+        </Layout>
+      </CartContextProvider>
+    </AuthContextProvider>
   );
 };
 
