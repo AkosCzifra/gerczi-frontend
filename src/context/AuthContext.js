27e0909d31a46e2746202c5629946ba99529bccc
gergicsmilan <gergics.milan95@gmail.com>
2020-06-16 13:13:54 +0200

    created auth context, jwt stored here

---
+++ src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+
+import { setJWTWithExpiry, getJWTWithExpiry, deleteJwt } from '../utils/jwt-manager/JwtManager';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = (props) => {
+  const jwtFromLocal = getJWTWithExpiry('token');
+  const [jwt, setJwt] = useState(jwtFromLocal);
+
+  const adjustJwt = (jwt) => {
+    setJWTWithExpiry('token', jwt, 86400000);
+    setJwt(jwt);
+  };
+
+  const logout = () => {
+    setJwt(null);
+    deleteJwt('token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ jwt: jwt, adjustJwt: adjustJwt, logout: logout }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContextProvider;
