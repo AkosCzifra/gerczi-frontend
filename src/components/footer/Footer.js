cb4159bc510d963cf280d5f00af32ad31a7b039b
gergicsmilan <gergics.milan95@gmail.com>
2020-06-24 22:49:13 +0200

    created basic footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledFooter = styled.footer`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 80px;
+  background-color: #121212;
 
+  p {
+    font-weight: bold;
+    font-size: 1.1rem;
+    color: #908870;
+  }
+`;
 
 const Footer = () => {
-    return (
-        <footer>Footer</footer>
-    )
-}
+  return (
+    <StyledFooter>
+      <p>© GerCzi Company</p>
+    </StyledFooter>
+  );
+};
 
 export default Footer;
