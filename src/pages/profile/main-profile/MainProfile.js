6afe4bf76f89b4c1512ebd5c61f1eb97aa83ac65
gergicsmilan <gergics.milan95@gmail.com>
2020-06-25 14:35:23 +0200

    created mainprofile component

---
+++ src/pages/profile/main-profile/MainProfile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const MainProfile = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pharetra lectus eget
+      ultricies egestas. Maecenas sit amet ligula ac ipsum hendrerit cursus quis quis justo. Proin
+      mollis nulla quam, nec mattis lacus condimentum nec. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Nulla fringilla tellus imperdiet posuere eleifend. Cras vel eleifend justo,
+      cursus sodales est. Vestibulum vitae purus nibh. Duis fermentum libero eget dolor mollis
+      iaculis. Integer ipsum nisl, scelerisque nec vestibulum nec, posuere ut ligula. Nulla
+      facilisis aliquet ligula, ut vehicula justo accumsan eget. Praesent aliquam sagittis odio,
+      vel eleifend eros placerat a. Vestibulum id euismod velit, eget bibendum risus. Nulla aliquet
+      ullamcorper dolor et congue. Nunc fermentum metus malesuada semper vestibulum. Nullam sit
+      amet nisi elit. Curabitur eget turpis felis. Cras et diam finibus, elementum libero eu,
+      fringilla quam. Pellentesque risus erat, fermentum ac sapien nec, dictum tincidunt lectus.
+      Sed tristique quis nisi a suscipit. Nulla ipsum odio, ullamcorper id lacus in, ultricies
+      tempor est. Maecenas dignissim cursus velit quis scelerisque. Morbi nec lacus eu est accumsan
+      rutrum. Etiam dolor tortor, lobortis vel erat id, lacinia viverra tortor. Ut ac turpis ante.
+      Sed elementum gravida lorem, blandit tristique ipsum vulputate vitae. Morbi tempor, metus vel
+      mattis interdum, risus diam eleifend felis, in pellentesque purus nunc et elit. Etiam laoreet
+      libero diam.
+    </div>
+  );
+};
+
+export default MainProfile;
